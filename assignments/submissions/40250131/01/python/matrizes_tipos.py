4423be6cc4ace5e9a2a6ba5f61e71b55ca996d66
Alice Alfredo <40250131@esmad.ipp.pt>
2026-04-07 11:41:21 -0400

    Submission 40250131 --- A01

--- assignments/submissions/40250131/01/python/matrizes_tipos.py
+++ assignments/submissions/40250131/01/python/matrizes_tipos.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def classificar_matriz(A):
+    """
+    Classificação de matrizes de acordo com o nº de linhas, nº de colunas e elementos.
+    """
     classificacoes = [] 
     lin, col = A.shape
 
@@ -31,28 +34,28 @@ def classificar_matriz(A):
 
 
 def main():
+    """
+    Matrizes
+    """
     zero = np.zeros((3, 4), dtype=int)
-
     identidade = np.eye(4, dtype=int)
-    
     diagonal = np.diag([2, 5, -1])
-
     sup_triangular = np.array(
         [[3, 2, 1], 
          [0, -1, 4], 
          [0, 0, 5]])
-    
     inf_triangular = np.array(
         [[3, 0, 0], 
          [2, -1, 0], 
          [1, 4, 5]])
-    
     M = np.array([[1, 2, 3], 
                   [0, -1, 4], [
                       5, 2, 0]])
-    
     simetrica = M + M.T
 
+    """
+    Print das classificações e atrizes na consola
+    """
     matrizes = [zero, identidade, diagonal, sup_triangular, inf_triangular, simetrica]
     nomes = ["Matriz Zero", "Matriz Identidade", "Matriz Diagonal", "Matriz Triangular Superior", "Matriz Triangular Inferior", "Matriz Simétrica"]
 
@@ -65,6 +68,9 @@ def main():
     A = np.zeros((2, 3))
     B = np.zeros((4, 2))
 
+    """
+    Verificação de erros para soma e multiplicação
+    """
     try:
         print(A + B)
     except ValueError as e:
